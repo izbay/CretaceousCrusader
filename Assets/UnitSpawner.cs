5d94b14182b459240d1289fa9d0533ff5cbeca71
coboba <brantrieder@gmail.com>
2015-04-05 00:10:41 -0400

    spawn points

---
+++ Assets/UnitSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitSpawner : MonoBehaviour {
+
+
+	public GameObject Lancer; 
+	public float TimetoSpawn = 2f;
+	public Transform[] spawnPoint;
+
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", TimetoSpawn, TimetoSpawn);
+		
+	}
+
+	void Spawn(){
+
+		int spawnPointArray = Random.Range (0, spawnPoint.Length);
+
+		Instantiate (Lancer, spawnPoint[spawnPointArray].position,spawnPoint[spawnPointArray].rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	
+	}
+}
