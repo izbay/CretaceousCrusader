225146cf7babf0b96da2ce753777e59502442fa9
izbay <jacobmartinek@knights.ucf.edu>
2015-04-23 03:24:21 -0400

    Win/Lose/Start Scenes
    All I have to do is hook em in.

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKey || Input.GetMouseButton (0) || Input.GetMouseButton (1)){
+			Application.LoadLevel ("Terrain");
+		}
+	}
+}
